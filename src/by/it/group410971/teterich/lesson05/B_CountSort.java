0d406b1a976b573bc5e4b8336664f27d347507b3
Nterentich <nikitateteric386@gmail.com>
2025-06-13 00:04:08 +0300

    добавил урок 5

--- src/by/it/group410971/teterich/lesson05/B_CountSort.java
+++ src/by/it/group410971/teterich/lesson05/B_CountSort.java
@@ -4,18 +4,7 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Scanner;
 
-/*
-Первая строка содержит число 1<=n<=10000, вторая - n натуральных чисел, не превышающих 10.
-Выведите упорядоченную по неубыванию последовательность этих чисел.
-
-При сортировке реализуйте метод со сложностью O(n)
-
-Пример: https://karussell.wordpress.com/2010/03/01/fast-integer-sorting-algorithm-on/
-Вольный перевод: http://programador.ru/sorting-positive-int-linear-time/
-*/
-
 public class B_CountSort {
-
 
     public static void main(String[] args) throws FileNotFoundException {
         InputStream stream = B_CountSort.class.getResourceAsStream("dataB.txt");
@@ -27,22 +16,27 @@ public class B_CountSort {
     }
 
     int[] countSort(InputStream stream) throws FileNotFoundException {
-        //подготовка к чтению данных
         Scanner scanner = new Scanner(stream);
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        //размер массива
         int n = scanner.nextInt();
         int[] points = new int[n];
 
-        //читаем точки
         for (int i = 0; i < n; i++) {
             points[i] = scanner.nextInt();
         }
-        //тут реализуйте логику задачи с применением сортировки подсчетом
 
+        int[] count = new int[11]; // так как числа не превышают 10
+        for (int num : points) {
+            count[num]++;
+        }
 
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        int index = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                points[index++] = i;
+                count[i]--;
+            }
+        }
+
         return points;
     }
-
 }
--- src/by/it/group410971/teterich/lesson05/C_QSortOptimized.java
+++ src/by/it/group410971/teterich/lesson05/C_QSortOptimized.java
@@ -2,32 +2,8 @@ package by.it.group410971.teterich.lesson05;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
+import java.util.Arrays;
 import java.util.Scanner;
-
-/*
-Видеорегистраторы и площадь 2.
-Условие то же что и в задаче А.
-
-        По сравнению с задачей A доработайте алгоритм так, чтобы
-        1) он оптимально использовал время и память:
-            - за стек отвечает элиминация хвостовой рекурсии
-            - за сам массив отрезков - сортировка на месте
-            - рекурсивные вызовы должны проводиться на основе 3-разбиения
-
-        2) при поиске подходящих отрезков для точки реализуйте метод бинарного поиска
-        для первого отрезка решения, а затем найдите оставшуюся часть решения
-        (т.е. отрезков, подходящих для точки, может быть много)
-
-    Sample Input:
-    2 3
-    0 5
-    7 10
-    1 6 11
-    Sample Output:
-    1 0 0
-
-*/
-
 
 public class C_QSortOptimized {
 
@@ -41,49 +17,115 @@ public class C_QSortOptimized {
     }
 
     int[] getAccessory2(InputStream stream) throws FileNotFoundException {
-        //подготовка к чтению данных
         Scanner scanner = new Scanner(stream);
-        //!!!!!!!!!!!!!!!!!!!!!!!!! НАЧАЛО ЗАДАЧИ !!!!!!!!!!!!!!!!!!!!!!!!!
-        //число отрезков отсортированного массива
         int n = scanner.nextInt();
         Segment[] segments = new Segment[n];
-        //число точек
         int m = scanner.nextInt();
         int[] points = new int[m];
         int[] result = new int[m];
 
-        //читаем сами отрезки
         for (int i = 0; i < n; i++) {
-            //читаем начало и конец каждого отрезка
-            segments[i] = new Segment(scanner.nextInt(), scanner.nextInt());
+            int start = scanner.nextInt();
+            int stop = scanner.nextInt();
+            segments[i] = new Segment(start, stop);
         }
-        //читаем точки
+
         for (int i = 0; i < m; i++) {
             points[i] = scanner.nextInt();
         }
-        //тут реализуйте логику задачи с применением быстрой сортировки
-        //в классе отрезка Segment реализуйте нужный для этой задачи компаратор
 
+        quickSort(segments, 0, segments.length - 1);
 
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
+        for (int i = 0; i < m; i++) {
+            int point = points[i];
+            int firstIndex = binarySearchFirst(segments, point);
+            if (firstIndex == -1) {
+                result[i] = 0;
+                continue;
+            }
+            int count = 0;
+            for (int j = firstIndex; j < segments.length && segments[j].start <= point; j++) {
+                if (point <= segments[j].stop) {
+                    count++;
+                }
+            }
+            result[i] = count;
+        }
+
         return result;
     }
 
-    //отрезок
-    private class Segment implements Comparable {
+    private void quickSort(Segment[] segments, int left, int right) {
+        while (left < right) {
+            int[] pivotIndices = partition(segments, left, right);
+            if (pivotIndices[0] - left < right - pivotIndices[1]) {
+                quickSort(segments, left, pivotIndices[0] - 1);
+                left = pivotIndices[1] + 1;
+            } else {
+                quickSort(segments, pivotIndices[1] + 1, right);
+                right = pivotIndices[0] - 1;
+            }
+        }
+    }
+
+    private int[] partition(Segment[] segments, int left, int right) {
+        int pivot = segments[left].start;
+        int i = left;
+        int j = right;
+        int k = left;
+
+        while (k <= j) {
+            if (segments[k].start < pivot) {
+                swap(segments, i, k);
+                i++;
+                k++;
+            } else if (segments[k].start > pivot) {
+                swap(segments, k, j);
+                j--;
+            } else {
+                k++;
+            }
+        }
+
+        return new int[]{i, j};
+    }
+
+    private void swap(Segment[] segments, int i, int j) {
+        Segment temp = segments[i];
+        segments[i] = segments[j];
+        segments[j] = temp;
+    }
+
+    private int binarySearchFirst(Segment[] segments, int point) {
+        int left = 0;
+        int right = segments.length - 1;
+        int result = -1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (segments[mid].start <= point) {
+                result = mid;
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+
+        return result;
+    }
+
+    private class Segment implements Comparable<Segment> {
         int start;
         int stop;
 
         Segment(int start, int stop) {
-            this.start = start;
-            this.stop = stop;
+            this.start = Math.min(start, stop);
+            this.stop = Math.max(start, stop);
         }
 
         @Override
-        public int compareTo(Object o) {
-            //подумайте, что должен возвращать компаратор отрезков
-            return 0;
+        public int compareTo(Segment o) {
+            return Integer.compare(this.start, o.start);
         }
     }
-
 }
