9f57925da79dda8b227acafce1a5dcaeae4fc47d
Nterentich <nikitateteric386@gmail.com>
2025-06-13 00:28:55 +0300

    добавил урок 8

--- src/by/it/group410971/teterich/lesson08/C_Stairs.java
+++ src/by/it/group410971/teterich/lesson08/C_Stairs.java
@@ -4,58 +4,36 @@ import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.util.Scanner;
 
-/*
-Даны число 1<=n<=100 ступенек лестницы и
-целые числа −10000<=a[1],…,a[n]<=10000, которыми помечены ступеньки.
-Найдите максимальную сумму, которую можно получить, идя по лестнице
-снизу вверх (от нулевой до n-й ступеньки), каждый раз поднимаясь на
-одну или на две ступеньки.
-
-Sample Input 1:
-2
-1 2
-Sample Output 1:
-3
-
-Sample Input 2:
-2
-2 -1
-Sample Output 2:
-1
-
-Sample Input 3:
-3
--1 2 1
-Sample Output 3:
-3
-
-*/
-
 public class C_Stairs {
 
-    int getMaxSum(InputStream stream ) {
+    int getMaxSum(InputStream stream) {
         Scanner scanner = new Scanner(stream);
-        int n=scanner.nextInt();
-        int stairs[]=new int[n];
+        int n = scanner.nextInt();
+        int[] stairs = new int[n];
         for (int i = 0; i < n; i++) {
-            stairs[i]=scanner.nextInt();
+            stairs[i] = scanner.nextInt();
         }
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     НАЧАЛО ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        int result = 0;
 
+        if (n == 0) return 0;
+        if (n == 1) return Math.max(0, stairs[0]);
 
+        // Массив для хранения максимальной суммы на каждой ступеньке
+        int[] dp = new int[n];
+        dp[0] = stairs[0];
+        dp[1] = Math.max(stairs[0] + stairs[1], stairs[1]);
 
+        for (int i = 2; i < n; i++) {
+            // Максимум из: пришли с i-1 ступеньки или с i-2 ступеньки
+            dp[i] = Math.max(dp[i-1], dp[i-2]) + stairs[i];
+        }
 
-        //!!!!!!!!!!!!!!!!!!!!!!!!!     КОНЕЦ ЗАДАЧИ     !!!!!!!!!!!!!!!!!!!!!!!!!
-        return result;
+        return Math.max(dp[n-1], dp[n-2]);
     }
-
 
     public static void main(String[] args) throws FileNotFoundException {
         InputStream stream = C_Stairs.class.getResourceAsStream("dataC.txt");
         C_Stairs instance = new C_Stairs();
-        int res=instance.getMaxSum(stream);
+        int res = instance.getMaxSum(stream);
         System.out.println(res);
     }
-
 }
